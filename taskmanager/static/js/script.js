58b976c642290c68d24cfd38007304d1d6c83eed
Darth Koder <N.Fisher21@outlook.com>
2024-08-04 13:13:41 +0000

    add add_task.html with functionality to add task name, description, due date, urgent option switch and pick a category drop down. Then add task submit button

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -10,3 +10,37 @@ document.addEventListener('DOMContentLoaded', function() {
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+
+
+  // Script for date picker
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "mmm dd, yyyy", 
+      i18n: {done: "Select"}
+    });
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+
+
+
+  // Script for selects
+
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
